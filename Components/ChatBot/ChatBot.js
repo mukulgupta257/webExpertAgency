13276541cf30572aa13e2429fb9c1bd9aa1b8804
Mukul Gupta <mukulgupta257@gmail.com>
2022-08-03 00:24:43 +0530

    Check

---
+++ Components/ChatBot/ChatBot.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Image from "next/image";
+
+export default function ChatBot() {
+  return (
+    <div className="ChatBot" id="chatBot">
+      <Image src={""} width="100%" height={"100%"} alt="ChatBot" />
+    </div>
+  );
+}
